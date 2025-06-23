a0661a76d1606074da4271a2a8fa67ec07782466
Raza <MohdRaza-Firoj-Deshpande>
2025-06-24 00:04:42 +0530

    & C:/Users/Cw/AppData/Local/Programs/Python/Python313/python.exe c:/Users/Cw/Desktop/python_work/hello/dictionaries.py

---
+++ hello/dictionaries.py
@@ -0,0 +1,11 @@
+# adding vaalues to variable
+
+a={'Student_name':'Raza','Student_class':10,'Student_roll':30}
+abc=a['Student_class']
+print(abc)
+
+# addinf new values to dictonaries
+a={'Student_name':'Raza','Student_class':10,'Student_roll':30}
+a['x']=10
+a['y']=20
+print(a)
